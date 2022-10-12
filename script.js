719547bfabc988e35acbf2125a645697c7860ced
강혁준 <gurwns232@naver.com>
2022-10-12 19:57:06 +0900

    class add remove js

---
+++ script.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector('.menu-icon span')
+const searchBtn = document.querySelector('.search-icon')
+const cancelBtn = document.querySelector('.cancel-icon')
+const items = document.querySelector('.nav-items')
+const form = document.querySelector('form')
+
+console.log(menuBtn)
+
+menuBtn.addEventListener('click', () => {
+    items.classList.add('active')
+    menuBtn.classList.add('hide')
+    searchBtn.classList.add('hide')
+    cancelBtn.classList.add('show')
+})
+
+cancelBtn.addEventListener('click', () => {
+    items.classList.remove('active')
+    menuBtn.classList.remove('hide')
+    searchBtn.classList.remove('hide')
+    cancelBtn.classList.remove('show')
+    form.classList.remove('active')
+
+    cancelBtn.style.color = '#ff3d00'
+})
+
+searchBtn.addEventListener('click', () => {
+    form.classList.add('active')
+    searchBtn.classList.add('hide')
+    cancelBtn.classList.add('show')
+})
